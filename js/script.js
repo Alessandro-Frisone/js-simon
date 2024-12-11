e8e8edcf31cd71d8073dd12ad8f234b4eb82b9b2
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-11 17:17:24 +0100

    fix: timer number remove hidden

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const num5Elm = document.getElementById("num5")
 const confirmElm = document.getElementById("confirm")
 const outputElm = document.getElementById("output")
 const randomNumberOutputElm = document.getElementById("randomNumberOutput")
+const timerElm = document.getElementById("timer")
 const boxNumbersElm = document.getElementById("boxNumbers")
 const inputUser1Elm = document.getElementById("inputUser1")
 const inputUser2Elm = document.getElementById("inputUser2")
@@ -25,12 +26,20 @@ function generateRandomNumbers() {
   const newNumbers = generateRandomNumbers();
   // Stampa dei numeri randomici a schermo
   randomNumberOutputElm.innerHTML = newNumbers;
-// Creazione timer (20 secondi)
-setTimeout(() => {
-  randomNumberOutputElm.classList.add("hidden")
-  inputUser1Elm.classList.remove("hidden")
-  inputUser2Elm.classList.remove("hidden")
-  inputUser3Elm.classList.remove("hidden")
-  inputUser4Elm.classList.remove("hidden")
-  inputUser5Elm.classList.remove("hidden")
-}, 5000);
+// Creazione timer (20 secondi) con comparsa input type e scomparsa numeri random
+let timeLeft = 5
+const timeInterval = setInterval(() => {
+  if (timeLeft === -1) {
+    clearInterval(timeInterval)
+    randomNumberOutputElm.classList.add("hidden")
+    inputUser1Elm.classList.remove("hidden")
+    inputUser2Elm.classList.remove("hidden")
+    inputUser3Elm.classList.remove("hidden")
+    inputUser4Elm.classList.remove("hidden")
+    inputUser5Elm.classList.remove("hidden")
+    timerElm.classList.add("hidden")
+  } else {
+    timerElm.innerHTML = timeLeft
+    timeLeft--
+  }
+}, 1000);
