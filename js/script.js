cabb0b08bda9a0766ee691485f8c38abedf8b629
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-11 16:38:26 +0100

    feat: remove random numbers

--- js/script.js
+++ js/script.js
@@ -5,13 +5,22 @@ const num4Elm = document.getElementById("num4")
 const num5Elm = document.getElementById("num5")
 const confirmElm = document.getElementById("confirm")
 const outputElm = document.getElementById("output")
+const randomNumberOutputElm = document.getElementById("randomNumberOutput")
+
 
 // Funzione per generare 5 numeri casuali
 function generateRandomNumbers() {
-    const numbers = [];
+    let numbers = [];
     for (let i = 0; i < 5; i++) {
       numbers.push(Math.floor(Math.random() * 100) + 1); 
     }
     return numbers;
   }
   const newNumbers = generateRandomNumbers();
+  // Stampa dei numeri randomici a schermo
+  randomNumberOutputElm.innerHTML = newNumbers;
+// Creazione timer (20 secondi)
+setTimeout(() => {
+  const finalNumbers = randomNumberOutputElm.classList.add("hidden")
+  const userNumbers = num1Elm.classList.remove("hidden")
+}, 5000);
